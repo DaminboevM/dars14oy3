aff901f8a7d45aeedc92a7d6b84a3e185b9760b0
DaminboevM <m701rizo@gmail.com>
2025-05-28 20:57:16 +0500

    alooooooo

--- src/service/Transport.service.js
+++ src/service/Transport.service.js
@@ -3,10 +3,12 @@ import { TransportModel } from "../module/transport.module.js"
 export class TransportService {
     constructor() {}
 
+    
     static async ReadTransport (branch_id) {
         const Transports = await TransportModel.findOne({branch_id})
         return {status: 201, messagae: 'Transport success Readed !', data: Transports, seccess: true}
     }
+
 
 
 
@@ -16,12 +18,14 @@ export class TransportService {
     }
     
     
+
     static async UpdateTransport (body, id) {
         await TransportModel.updateOne({_id: id, $set: body})
         return {status: 201, messagae: 'Transport success Updated !', data: null, seccess: true}
     }
     
     
+
     static async DeleteTransport (id) {
         await TransportModel.findByIdAndDelete(id)
         return {status: 201, messagae: 'Transport success Deleted !', data: null, seccess: true}
