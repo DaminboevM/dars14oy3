c514b9e4f45add071b53dcf70faa76dac9f837f7
DaminboevM <m701rizo@gmail.com>
2025-05-28 22:10:54 +0500

    !

--- src/app.js
+++ src/app.js
@@ -5,13 +5,16 @@ import errorHandler from './middleware/errHeandler.js'
 import router from './router/router.js'
 import fileUpload from 'express-fileupload'
 
+
 const app = express()
 const PORT = process.env.PORT || 4000
+
 
 app.use(express.json())
 app.use(fileUpload())
 await connectDB()
 app.use('/v1/api',router)
+
 
 app.use(errorHandler)
 
